8cf9cf8c7a94c85b08f9fbdd80cb29dfd0b1881d
MrAdhit <adhityarimba97@gmail.com>
2022-08-10 22:04:08 +0700

    test

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
 const fs = require("fs");
 const EventEmitter = require("events");
 const fss = fs.promises;
 const csv = require("csv-parser");
 const log4js = require("log4js");
+const userAgent = require("random-useragent");
 
 const config = require("./config.json");
-const eventEmitter = new EventEmitter();
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 const date = new Date().toISOString().replace(/:/g, "-");
 let id = 0;
 
@@ -29,6 +30,7 @@ log4js.configure({
     }
 });
 config.maxBrowser--;
+puppeteer.use(StealthPlugin());
 
 let browserList = new Map();
 
@@ -61,11 +63,55 @@ async function openBrowser(input, id){
     logger.addContext("browserID", date);
 
     logger.info(`Membuka Browser`);
-    const browser = await puppeteer.launch({headless: false, args: ["--disable-site-isolation-trials"]});
+    const browser = await puppeteer.launch({headless: false, ignoreDefaultArgs: ["--enable-automation", "--enable-blink-features=IdleDetection"], args: ["--window-size=1920,1080", "--no-sandbox"], defaultViewport: null});
     browserList.set(id, true);
-
     try{
         const page = await browser.newPage();
+        await page.setRequestInterception(true);
+        page.on("request", (request) => {
+            const url = request.url();
+
+            const filters = [
+              "btstatic",
+              "googleadservices",
+              "doubleclick",
+              "idsync",
+              "quant",
+              "facebook",
+              "amazon",
+              "tracking",
+              "taboola",
+              ".gif",
+              "google-analytics",
+              "forter",
+              "event",
+              "analytics",
+              "client-log",
+              "images"
+            ];
+
+            const whitelist = [
+                "tokopedia.com",
+                "tokopedia.net"
+            ]
+      
+            const shouldAbort = whitelist.some(
+              (urlPart) => url.includes(urlPart)
+            );
+            const shouldAbort2 = filters.some(
+                (urlPart) => url.includes(urlPart)
+            )
+
+            if (!shouldAbort) request.abort();
+            else if (shouldAbort2) request.abort();
+            else {
+              request.continue();
+            }
+          });
+
+        let uag = userAgent.getRandom((ua) => ua.osName == "Windows" && ua.browserName == "Firefox");
+        logger.info(`Mengatur useragent menjadi "${uag}"`);
+        await page.setUserAgent(uag);
 
         logger.info(`Mengatur Cookies`);
         let cookies = await fss.readFile(input.akun).catch((err) => {
@@ -73,6 +119,7 @@ async function openBrowser(input, id){
             browser.close();
             return;
         });
+
         try{
             cookies = JSON.parse(cookies);
         }catch(error){
@@ -87,17 +134,34 @@ async function openBrowser(input, id){
         logger.info(`Sukses menuju "${input.produk}"`);
 
         let isVariant = true;
+        let stockAvailable = false;
         let variantType = 1;
         let note = (typeof(input.note) == "undefined" | input.note == "" ? config.note : input.note);
         let jumlahCustom = (typeof(input.jumlah) == "undefined" | input.jumlah <= 1 | input.jumlah == "" ? false : true);
 
         try{
+            logger.info(`Mengecek jenis varian`);
             await page.waitForSelector("#pdpVariantContainer > div", {timeout: config.defaultTimeout});
         }catch(error){
             isVariant = false;
         }
 
+        try{
+            logger.info(`Mengecek stock`);
+            await page.waitForSelector("#unf-ticker__active-item > div:nth-child(2) > p.css-1ih5x8y-unf-heading.e1qvo2ff8", {timeout: config.defaultTimeout});
+        }catch(error){
+            stockAvailable = true;
+        }
+
         logger.info((isVariant ? "Produk memiliki varian" : "Produk tidak memiliki varian"));
+
+        if(!stockAvailable){
+            logger.warn(`Stock produk telah habis`);
+            logger.warn(`Menutup browser`);
+            browserList.delete(id);
+            browser.close();
+            return;
+        }
 
         if(isVariant){
             try{
@@ -134,25 +198,44 @@ async function openBrowser(input, id){
             await page.waitForTimeout(1000);
             await page.waitForSelector("#pdpFloatingActions > div.css-1gqhae2 > button", {timeout: config.defaultTimeout});
             await page.click("#pdpFloatingActions > div.css-1gqhae2 > button");
-            await page.waitForTimeout(1000);
+            await page.waitForTimeout(2000);
             if(jumlahCustom){
+                let stock = await page.$("#pdpFloatingActions > div.css-1gqhae2 > div > div.css-qfdk7t > p > b");
+                stock = await stock.evaluate(el => el.textContent);
+                stock = parseInt(stock.replace().replace(/\D/g, ""));
+                await page.waitForTimeout(1000);
+                if(parseInt(input.jumlah) > stock) {
+                    logger.warn(`Jumlah stock lebih sedikit daripada jumlah yang akan dibeli`);
+                    input.jumlah = "1";
+                    logger.warn(`Mengubah jumlah yang akan dibeli menjadi 1`);
+                }
+                await page.waitForTimeout(1000);
                 logger.info(`Mengatur jumlah menjadi ${input.jumlah}`);
                 await page.waitForSelector("#pdpFloatingActions > div.css-1gqhae2 > div > div.css-qfdk7t > div > input", {timeout: config.defaultTimeout});
                 await page.click("#pdpFloatingActions > div.css-1gqhae2 > div > div.css-qfdk7t > div > input");
-                await page.keyboard.down("ControlLeft");
-                await page.keyboard.press("a");
-                await page.keyboard.up("ControlLeft");
-                await page.keyboard.press("Backspace");
-                await page.type("#pdpFloatingActions > div.css-1gqhae2 > div > div.css-qfdk7t > div > input", input.jumlah);
+                for (let index = 1; index < (input.jumlah - 2); index++) {
+                    await page.waitForTimeout(500);
+                    await page.click("#pdpFloatingActions > div.css-1gqhae2 > div > div.css-qfdk7t > div > button:nth-child(3)");
+                }
             }
-            let xpath1 = await page.$x("/html/body/div[1]/div/div[2]/div[2]/div[3]/div/div[1]/div[2]/div/div[3]/a");
-            xpath1[0].click();
+            await page.waitForTimeout(1000);
+            await page.waitForSelector("#pdpFloatingActions > div.css-1gqhae2 > div > div.css-1qxp37q > a > b", {timeout: config.defaultTimeout});
+            await page.click("#pdpFloatingActions > div.css-1gqhae2 > div > div.css-1qxp37q > a > b");
             await page.waitForTimeout(1000);
             logger.info(`Mengisi note dengan "${note}"`);
             await page.waitForSelector("#pdpFloatingActions > div.css-1gqhae2 > div > div.css-1qxp37q > input");
             await page.type("#pdpFloatingActions > div.css-1gqhae2 > div > div.css-1qxp37q > input", note);
         }else{
             if(jumlahCustom){
+                let stock = await page.$("#pdpFloatingActions > div.css-qfdk7t > p > b");
+                stock = await stock.evaluate(el => el.textContent);
+                stock = parseInt(stock.replace().replace(/\D/g, ""));
+                if(parseInt(input.jumlah) > stock) {
+                    logger.warn(`Jumlah stock lebih sedikit daripada jumlah yang akan dibeli`);
+                    input.jumlah = "1";
+                    logger.warn(`Mengubah jumlah yang akan dibeli menjadi 1`);
+                }
+                await page.waitForTimeout(1000);
                 logger.info(`Mengatur jumlah menjadi ${input.jumlah}`);
                 await page.waitForSelector("#pdpFloatingActions > div.css-qfdk7t > div > input", {timeout: config.defaultTimeout});
                 await page.click("#pdpFloatingActions > div.css-qfdk7t > div > input");
@@ -161,27 +244,52 @@ async function openBrowser(input, id){
                 await page.keyboard.up("ControlLeft");
                 await page.keyboard.press("Backspace");
                 await page.type("#pdpFloatingActions > div.css-qfdk7t > div > input", input.jumlah);
+                for (let index = 1; index < (input.jumlah - 2); index++) {
+                    await page.waitForTimeout(500);
+                    await page.click("#pdpFloatingActions > div.css-qfdk7t > div > button:nth-child(3)");
+                }
             }
-            logger.info(`Mengisi note dengan "${note}"`);
+            await page.waitForTimeout(1000);
             await page.waitForSelector("#pdpFloatingActions > div.css-1qxp37q > a", {timeout: config.defaultTimeout});
             await page.click("#pdpFloatingActions > div.css-1qxp37q > a");
             await page.waitForTimeout(1000);
+            logger.info(`Mengisi note dengan "${note}"`);
             await page.waitForSelector("#pdpFloatingActions > div.css-1qxp37q > input");
             await page.type("#pdpFloatingActions > div.css-1qxp37q > input", note);
         }
+        
+        let isSuccess = false;
+        let tryAtc = 0;
 
+        while(!isSuccess){
+            logger.info(`Mencoba Add to cart (${tryAtc})`);
+            await page.waitForTimeout(1000);
+            await page.waitForSelector("#pdpFloatingActions > div.css-qiunk2 > div.css-c1hnei > button.css-ra3v66-unf-btn.eg8apji0");
+            await page.click("#pdpFloatingActions > div.css-qiunk2 > div.css-c1hnei > button.css-ra3v66-unf-btn.eg8apji0");
+            let v1 = false;
+            try{
+                await page.waitForSelector(".css-jtcihq-unf-heading", {timeout: config.defaultTimeout});
+                let errorMsg = await page.$(".css-jtcihq-unf-heading");
+                errorMsg = await errorMsg.evaluate(el => el.textContent);
+                logger.error(errorMsg);
+                if(errorMsg == "Maaf, terjadi sedikit kendala. Coba ulangi beberapa saat lagi ya."){
+                    v1 = true;
+                    tryAtc++;
+                    await page.waitForTimeout(10000);
+                }
+            }catch(error){
+                
+            }
+            isSuccess = !v1;
+        }
+        // await page.screenshot({path: `./ssLogs/${date}-${id}.png`});
         logger.info("Selesai");
         browserList.delete(id);
-        id--;
-        await page.waitForTimeout(1000);
-        await page.waitForSelector("#pdpFloatingActions > div.css-qiunk2 > div.css-c1hnei > button.css-ra3v66-unf-btn.eg8apji0");
-        await page.click("#pdpFloatingActions > div.css-qiunk2 > div.css-c1hnei > button.css-ra3v66-unf-btn.eg8apji0");
-        await page.waitForTimeout(2000);
-        await browser.close();    
+        await browser.close();
     }catch(error){
         logger.error(error);
         browser.close();
-        logger.info("Mengalami kegagalan, menjalankan ulang!");
+        logger.warn("Mengalami kegagalan, menjalankan ulang!");
         openBrowser(input, id);
     }
 }
